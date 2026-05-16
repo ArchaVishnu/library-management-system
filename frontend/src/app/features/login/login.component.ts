5ab74c2b1f49e82392bb04cd542c3495b5a4716c
Archa Mohan <archamohan2801@gmail.com>
2026-05-16 10:53:05 +0530

    Change theme and update few stylings

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -1,18 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { Router, ActivatedRoute } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TranslatePipe } from '../../core/pipes/translate.pipe';
 
 @Component({
   selector: 'lms-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    TranslatePipe,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {}
 
   public onLoginSuccess(): void {
-    const returnUrl = this.activatedRoute.snapshot.queryParamMap.get('returnUrl') || '/dashboard';
+    const returnUrl =
+      this.activatedRoute.snapshot.queryParamMap.get('returnUrl') ||
+      '/dashboard';
     this.router.navigateByUrl(returnUrl);
   }
 }
